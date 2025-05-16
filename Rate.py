bf2f1f4b80c59d10d7b5d9feffaa5d158a4813bf
unknown <kostya.d.ivanov@gmail.com>
2025-05-16 23:36:56 +0300

    Correct euro

--- Rate.py
+++ Rate.py
@@ -2,25 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 url_usd = 'https://www.banki.ru/products/currency/usd/'
-url_evro = 'https://www.banki.ru/products/currency/cash/eur/moskva/'
+url_euro = 'https://www.banki.ru/products/currency/cash/eur/moskva/'
 
 response_usd = requests.get(url_usd)
 soup_usd = BeautifulSoup(response_usd.text, 'lxml')
 
-response_evro = requests.get(url_evro)
-soup_evro = BeautifulSoup(response_evro.text, 'lxml')
+response_euro = requests.get(url_euro)
+soup_euro = BeautifulSoup(response_euro.text, 'lxml')
 
 
 data_dollar = float(soup_usd.find('div', class_= 'Text__sc-vycpdy-0 jBQTbF').text.replace('₽', '').replace(',', '.'))
-data_evro = float(soup_evro.find('div', class_= 'Text__sc-vycpdy-0 jBQTbF').text.replace('₽', '').replace(',', '.'))
+data_euro = float(soup_euro.find('div', class_= 'Text__sc-vycpdy-0 jBQTbF').text.replace('₽', '').replace(',', '.'))
 
 print(f'    Exchange rate:')
 print(f'    1 $ = {data_dollar} ₽')
-print(f'    1 € = {data_evro} ₽')
+print(f'    1 € = {data_euro} ₽')
 print('---------------------')
 
-rate_evro_to_dollar = round(data_evro/data_dollar, 2)
-rate_dollar_to_evro = round(data_dollar/data_evro, 2)
+rate_euro_to_dollar = round(data_euro/data_dollar, 2)
+rate_dollar_to_euro = round(data_dollar/data_euro, 2)
 
 while True:
     currency = input('Enter the currency: ').lower()
@@ -28,23 +28,23 @@ while True:
     if currency == 'dollar':
         count = float(input('Amount in dollars: '))
         result_rub = round(data_dollar *  count, 2)
-        result_evro = round(count * rate_dollar_to_evro, 2)
+        result_euro = round(count * rate_dollar_to_euro, 2)
         print(f'Amount in rubles: {result_rub}')
-        print(f'Amount in evro: {result_evro}')
+        print(f'Amount in euro: {result_euro}')
 
-    elif currency == 'evro':
-        count = float(input('Amount in evro: '))
-        result_rub = round(data_evro * count, 2)
-        result_dollar = round(rate_evro_to_dollar * count, 2)
+    elif currency == 'euro':
+        count = float(input('Amount in euro: '))
+        result_rub = round(data_euro * count, 2)
+        result_dollar = round(rate_euro_to_dollar * count, 2)
         print(f'Amount in rubles: {result_rub}')
         print(f'Amount in dollars: {result_dollar}')
 
     elif currency == 'rubles':
         count = int(input('Amount in rubles: '))
         result_dollar = round(count/data_dollar, 2)
-        result_evro = round(count/data_evro, 2)
+        result_euro = round(count/data_euro, 2)
         print(f'Amount in dollars: {result_dollar}')
-        print(f'Amount in evro: {result_evro} ')
+        print(f'Amount in euro: {result_euro} ')
 
     elif currency == 'exit':
         break
