4dba7afd4b12855b4f6f3fe8a95c68c09ead32f0
unknown <kostya.d.ivanov@gmail.com>
2025-05-31 22:20:44 +0300

    BOld

--- Rate.py
+++ Rate.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import tkinter as tk
 from tkinter import ttk
-
+import tkinter.font as font
 url_usd = 'https://www.banki.ru/products/currency/usd/'
 url_euro = 'https://www.banki.ru/products/currency/cash/eur/moskva/'
 
@@ -23,7 +23,8 @@ root.title("Currency Converter")
 root.geometry("500x400")  
 
 currensy = ['Dollar', 'Euro', 'Ruble']
-default_font = ("Arial", 10)
+default_font = ('Arial', 10)
+default_font_bold = font.Font(family='Arial',size=10, weight ='bold')
 
 bottom_frame = tk.Frame(root)
 bottom_frame.pack(side='bottom', pady=10)
@@ -46,16 +47,16 @@ result_label_to.pack()
 combobox2 = ttk.Combobox(values=currensy, width=28, font=default_font)
 combobox2.pack(pady=5)
 
-result_label_title = tk.Label(root, text='RATE:',font=default_font)
+result_label_title = tk.Label(root, text='RATE:',font=default_font_bold)
 result_label_title.pack()
 result_label_dollar = tk.Label(root, text=f'1$ = {data_dollar}₽',font=default_font)
 result_label_euro = tk.Label(root,text=f'1€ = {data_euro}₽',font=default_font)
 result_label_dollar.pack(pady=(5,0))
 result_label_euro.pack(pady=(5,0))
 
-result_label_title = tk.Label(root, text='RESULT:',font=default_font)
+result_label_title = tk.Label(root, text='RESULT:',font=default_font_bold)
 result_label_title.pack()
-result_output = tk.Label(root, text='',font=default_font)
+result_output = tk.Label(root, text='',font=default_font_bold)
 result_output.pack()
 
 
